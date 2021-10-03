2adf65e89f8f95bfc6eb4ad3a51f1f7f1d37a406
luizas <luizasanchez.la@gmail.com>
2021-10-03 12:47:21 -0400

    created third file

---
+++ src/setup/Third.java
@@ -0,0 +1,14 @@
+package setup;
+
+public class Third {
+
+    public void a(String s){
+        System.out.println("New file");
+    }
+    public void dog (int Num){
+        System.out.println("Dog Breed");
+    }
+    public void cat (int Color) {
+        System.out.println("Cat color");
+    }
+}
